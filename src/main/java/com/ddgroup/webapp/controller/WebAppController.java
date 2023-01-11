0ed2195c11855310f1387e048068f407f477582b
mdziewanowski <m.dziewanowski@ambsoft.pl>
2023-01-11 13:10:16 +0100

    init

---
+++ src/main/java/com/ddgroup/webapp/controller/WebAppController.java
@@ -0,0 +1,15 @@
+package com.ddgroup.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebAppController {
+
+    @GetMapping("/")
+    String index(Model model) {
+        model.addAttribute("welcomeMessage", "Service Works");
+        return "index";
+    }
+}
